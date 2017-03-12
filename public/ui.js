994ccd3b3a281f00e1d3650d423624e971d2d65e
Jonathan Keane <jkeane@gmail.com>
2017-03-11 18:51:55 -0600

    Added instructions dismisal by clicking anywhere that is not inside of the instructions.

--- public/ui.js
+++ public/ui.js
@@ -797,6 +797,31 @@ function ui_showinstructions(job)
     instructions(job, h)
 
     ui_disable();
+
+    document.body.addEventListener("click", function(e) {
+      var target = e.target || e.srcElement;
+      var instructions_area = document.getElementById("instructionsdialog");
+      var instructions_button = document.getElementById("instructionsbutton");
+
+      // if the clicked object is in the instructions area or the instructions
+      // button itself ignore the click
+      if ( ( instructions_area != null && target !== instructions_area && !isChildOf(target, instructions_area) ) &&
+           ( instructions_button != null && target !== instructions_button && !isChildOf(target, instructions_button) ) ) {
+             ui_closeinstructions();
+             console.log("You clicked outside the content area!");
+      }
+    }, false);
+
+}
+
+function isChildOf(child, parent) {
+  if (child.parentNode === parent) {
+    return true;
+  } else if (child.parentNode === null) {
+    return false;
+  } else {
+    return isChildOf(child.parentNode, parent);
+  }
 }
 
 function ui_closeinstructions()
